d341b6caf488ab6384a16a120f1697246f373f07
aarshpatel1 <aarsh18patel@gmail.com>
2025-01-25 13:00:47 +0530

    improve styling of productDetails

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import products from "../data/productsData"
+import products from "../data/productsData";
 
 function ProductDetails() {
 	const { productId } = useParams();
@@ -10,25 +10,31 @@ function ProductDetails() {
 
 	return (
 		<>
-			<div className="container my-5">
-				<div className="row">
-					<div className="col-md-6">
-						<img src={product.imageUrl} className="img-fluid" alt="Product" />
+			<div className="my-5 shadow">
+				<div className="row" style={{ height: "400px" }}>
+					<div className="col-md-6 d-flex justify-content-center align-items-center">
+						<img
+							src={product.imageUrl}
+							className="img-fluid h-100"
+							alt="Product"
+						/>
 					</div>
-					<div className="col-md-6">
-						<h2>Product {productId}</h2>
-						<p>
+					<div className="col-md-6 d-flex flex-column justify-content-center align-items-start">
+						<h2 className="mb-3">Product {productId}</h2>
+						<p className="my-3">
 							Product description goes here. It provides details about the
 							product features, specifications, and other relevant information.
 						</p>
-						<h4>$99.99</h4>
-						<button
-							className="btn btn-outline-secondary me-2"
-							onClick={() => navigate(-1)}
-						>
-							Go Back
-						</button>
-						<button className="btn btn-success">Add to Cart</button>
+						<h4 className="my-3">$99.99 <del className="ms-3">$109.99</del></h4>
+						<div className="d-flex align-content-center my-3">
+							<button
+								className="btn btn-outline-secondary me-3"
+								onClick={() => navigate(-1)}
+							>
+								Go Back
+							</button>
+							<button className="btn btn-success">Add to Cart</button>
+						</div>
 					</div>
 				</div>
 			</div>
