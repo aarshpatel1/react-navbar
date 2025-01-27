31d396339cdf74aa06bac191c9235a981c463a54
aarshpatel1 <aarsh18patel@gmail.com>
2025-01-27 22:36:29 +0530

    add register form and login form design

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,44 @@ import React from "react";
 function Login() {
 	return (
 		<>
-			<h1 className="my-5 text-center">Login</h1>
+			<div className="d-flex flex-column justify-content-center align-items-center">
+				<h1 className="my-5 text-center">Login</h1>
+				<form method="POST" className="p-5 rounded-4">
+					<div className="mb-3">
+						<label htmlFor="email" className="form-label">
+							Email :
+						</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							className="form-control"
+							required
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="password" className="form-label">
+							Password :
+						</label>
+						<input
+							type="password"
+							id="password"
+							name="password"
+							className="form-control"
+							required
+						/>
+					</div>
+
+					<p className="form-text my-4">
+						If you haven't registered, <a href="/register">register here! </a>
+					</p>
+					<div className="d-flex align-items-center justify-content-center">
+						<button type="submit" className="btn btn-secondary mt-3">
+							Login
+						</button>
+					</div>
+				</form>
+			</div>
 		</>
 	);
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,7 +3,57 @@ import React from "react";
 function Register() {
 	return (
 		<>
-			<h1 className="my-5 text-center">Register</h1>
+			<div className="d-flex flex-column justify-content-center align-items-center">
+				<h1 className="my-5 text-center">Register</h1>
+				<form method="POST" className="p-5 rounded-4">
+					<div className="mb-3">
+						<label htmlFor="email" className="form-label">
+							Email :
+						</label>
+						<input
+							type="email"
+							id="email"
+							name="email"
+							className="form-control"
+							required
+						/>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="password" className="form-label">
+							Password :
+						</label>
+						<input
+							type="password"
+							id="password"
+							name="password"
+							className="form-control"
+							required
+						/>
+					</div>
+
+					<div className="mb-3">
+						<label htmlFor="confirm_password" className="form-label">
+							Confirm Password :
+						</label>
+						<input
+							type="password"
+							id="confirm_password"
+							name="confirm_password"
+							className="form-control"
+							required
+						/>
+					</div>
+
+					<p className="form-text my-4">
+						If you have already registered, <a href="/login">login here! </a>
+					</p>
+					<div className="d-flex align-items-center justify-content-center">
+						<button type="submit" className="btn btn-secondary mt-3">
+							Register
+						</button>
+					</div>
+				</form>
+			</div>
 		</>
 	);
 }
